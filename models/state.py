b6cba3f6dbb6666fd788bff67f6d5e7514014a58
Fiegsy <yassine.daoui031@gmail.com>
2024-03-28 08:49:41 -0700

    Updated state file that inherit from base model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module for HBNB project."""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class State(BaseModel, Base):
+    """State class for the HBNB project."""
+    __tablename__ = "states"
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state", cascade="all, delete")
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter attribute.
+            Returns the list of City instances with state_id matching
+            the current State.id"""
+            from models import storage
+
+            my_cities = []
+            for city in storage.all(City).values():
+                if self.id == city.state_id:
+                    my_cities.append(city)
+            return my_cities
