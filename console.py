0bab693f2d07add63e4a33fd07af08c9596a49f7
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 06:40:30 -0700

    Updated back to the working code with indetention fixed

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
+""" Custom Console Module """
 import cmd
 import sys
-import shlex
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -11,48 +12,107 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-class HBNBCommand(cmd.Cmd):
-    """Contains the functionality for the HBNB console"""
+class CustomConsole(cmd.Cmd):
+    """ Customized Console Interface """
 
-    
-    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    # Prompt definition for interactive/non-interactive modes
+    prompt = '(custom-console) ' if sys.__stdin__.isatty() else ''
 
+    # Dictionary to store class names and their corresponding classes
     classes = {
-        'BaseModel': BaseModel, 'User': User, 'Place': Place,
-        'State': State, 'City': City, 'Amenity': Amenity,
-        'Review': Review
-    }
+               'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
+              }
 
-    
+    # List of dot commands
+    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
 
-    def do_create(self, args):
-        """Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
+    # Dictionary to store attribute types
+    types = {
+             'number_rooms': int, 'number_bathrooms': int,
+             'max_guest': int, 'price_by_night': int,
+             'latitude': float, 'longitude': float
+            }
 
-        arg_list = shlex.split(args)
+    def preloop(self):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(custom-console)')
 
-        class_name = arg_list[0]
-        if class_name not in self.classes:
-            print("** class doesn't exist **")
-            return
+    def precmd(self, line):
+        """Reformat command line for advanced command syntax."""
+        _cmd = _cls = _id = _args = ''  # initialize line elements
 
-       
-        params = {}
-        for param in arg_list[1:]:
-            key, value = param.split('=')
-            if value.startswith('"') and value.endswith('"'):
-                value = value[1:-1]  
-                value = value.replace('_', ' ')  
-                params[key] = value
+        # scan for general formatting - i.e '.', '(', ')'
+        if not ('.' in line and '(' in line and ')' in line):
+            return line
 
-        
-        obj = self.classes[class_name](**params)
-        obj.save()
-        print(obj.id)
+        try:  # parse line left to right
+            pline = line[:]  # parsed line
+
+            # isolate <class name>
+            _cls = pline[:pline.find('.')]
+
+            # isolate and validate <command>
+            _cmd = pline[pline.find('.') + 1:pline.find('(')]
+            if _cmd not in CustomConsole.dot_cmds:
+                raise Exception
+
+            # if parentheses contain arguments, parse them
+            pline = pline[pline.find('(') + 1:pline.find(')')]
+            if pline:
+                # partition args: (<id>, [<delim>], [<*args>])
+                pline = pline.partition(', ')  # pline convert to tuple
+
+                # isolate _id, stripping quotes
+                _id = pline[0].replace('\"', '')
+
+                # if arguments exist beyond _id
+                pline = pline[2].strip()  # pline is now str
+                if pline:
+                    # check for *args or **kwargs
+                    if pline[0] == '{' and pline[-1] == '}'\
+                            and type(eval(pline)) is dict:
+                        _args = pline
+                    else:
+                        _args = pline.replace(',', '')
+
+            line = ' '.join([_cmd, _cls, _id, _args])
+
+        except Exception as mess:
+            pass
+        finally:
+            return line
+
+    def postcmd(self, stop, line):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(custom-console) ', end='')
+        return stop
+
+    def do_quit(self, command):
+        """ Exit the custom console"""
+        exit()
+
+    def help_quit(self):
+        """ Prints the help documentation for quit  """
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        exit()
+
+    def help_EOF(self):
+        """ Prints the help documentation for EOF """
+        print("Exits the program without formatting\n")
+
+    def emptyline(self):
+        """ Overrides the emptyline method of CMD """
+        pass
 
     
 
 if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+    CustomConsole().cmdloop()
