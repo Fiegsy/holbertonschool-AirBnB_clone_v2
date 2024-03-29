29129eb01f6943d1e28efc6e87a91297df0c2e0d
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 06:43:12 -0700

    Updated file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,98 +1,57 @@
-from datetime import datetime
-import uuid
-from sqlalchemy import Column, DateTime, String
-from sqlalchemy.ext.declarative import declarative_base
+#!/usr/bin/python3
+"""Module for managing file storage in a different way"""
+import json
 
-Base = declarative_base()
 
-class BaseModel(Base):
-    """A base class for all hbnb models"""
-    __tablename__ = 'base_models'
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
-        return dictionary
-
-    def delete(self):
-        """Deletes the current instance from storage"""
-        from models import storage
-        storage.delete(self)
-
-class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+class DifferentFileStorage:
+    """Manages storage of hbnb models using a custom approach"""
+    _file_path = 'different_file.json'
+    _objects = {}
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
-            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
-        return self.__objects
+        if cls is not None:
+            filtered_objects = {}
+
+            for key, obj in DifferentFileStorage._objects.items():
+                if isinstance(obj, cls):
+                    filtered_objects[key] = obj
+
+            return filtered_objects
+        else:
+            return DifferentFileStorage._objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Adds a new object to storage"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        DifferentFileStorage._objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+        with open(DifferentFileStorage._file_path, 'w') as file:
+            serialized_objs = {key: obj.to_dict() for key, obj in DifferentFileStorage._objects.items()}
+            json.dump(serialized_objs, file)
 
     def reload(self):
         """Loads storage dictionary from file"""
         try:
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                self.__objects = {}
-                for key, val in data.items():
-                    class_name = val['__class__']
-                    if class_name in self.classes:
-                        obj = self.classes[class_name](**val)
-                        self.__objects[key] = obj
+            with open(DifferentFileStorage._file_path, 'r') as file:
+                serialized_objs = json.load(file)
+                for key, obj_data in serialized_objs.items():
+                    cls_name, obj_id = key.split('.')
+                    cls = eval(cls_name)
+                    obj = cls(**obj_data)
+                    DifferentFileStorage._objects[key] = obj
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes an object from __objects if it's inside"""
-        if obj:
+        """Deletes an object from storage"""
+        if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
+            if key in DifferentFileStorage._objects:
+                del DifferentFileStorage._objects[key]
 
     def close(self):
-        """Deserializes the JSON file to objects"""
+        """Closes the storage"""
         self.reload()
