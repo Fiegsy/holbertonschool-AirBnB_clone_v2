b1c7bd40542d6918dfe440fd29532301021985b9
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 06:45:01 -0700

    Added the deleted method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,56 +1,56 @@
 #!/usr/bin/python3
-"""Module for managing file storage in a different way"""
+"""Module for managing file storage"""
 import json
 
 
-class DifferentFileStorage:
-    """Manages storage of hbnb models using a custom approach"""
-    _file_path = 'different_file.json'
-    _objects = {}
+class FileStorage:
+    """Manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
             filtered_objects = {}
 
-            for key, obj in DifferentFileStorage._objects.items():
+            for key, obj in FileStorage.__objects.items():
                 if isinstance(obj, cls):
                     filtered_objects[key] = obj
 
             return filtered_objects
         else:
-            return DifferentFileStorage._objects
+            return FileStorage.__objects
 
     def new(self, obj):
-        """Adds a new object to storage"""
+        """Adds new object to storage dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        DifferentFileStorage._objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(DifferentFileStorage._file_path, 'w') as file:
-            serialized_objs = {key: obj.to_dict() for key, obj in DifferentFileStorage._objects.items()}
+        with open(FileStorage.__file_path, 'w') as file:
+            serialized_objs = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
             json.dump(serialized_objs, file)
 
     def reload(self):
         """Loads storage dictionary from file"""
         try:
-            with open(DifferentFileStorage._file_path, 'r') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 serialized_objs = json.load(file)
                 for key, obj_data in serialized_objs.items():
                     cls_name, obj_id = key.split('.')
                     cls = eval(cls_name)
                     obj = cls(**obj_data)
-                    DifferentFileStorage._objects[key] = obj
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes an object from storage"""
+        """Deletes an object from __objects if it's inside"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in DifferentFileStorage._objects:
-                del DifferentFileStorage._objects[key]
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
 
     def close(self):
         """Closes the storage"""
