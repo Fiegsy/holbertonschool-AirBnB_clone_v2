ac2f221148ae8d259db47334e8c4202ee50077e9
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 06:28:41 -0700

    Fixed methods to produce the correct output

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,62 @@
+from datetime import datetime
+import uuid
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    """A base class for all hbnb models"""
+    __tablename__ = 'base_models'
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
+        return dictionary
+
+    def delete(self):
+        """Deletes the current instance from storage"""
+        from models import storage
+        storage.delete(self)
+
+
 import json
-import os
-import uuid  
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
-
-class CustomFileStorage:
+class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'custom_file.json'
+    __file_path = 'file.json'
     __objects = {}
-
-    def __init__(self):
-        self.classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -29,9 +65,7 @@ class CustomFileStorage:
         return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary with a new ID"""
-        if not obj.id:
-            obj.id = str(uuid.uuid4()) 
+        """Adds new object to storage dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
