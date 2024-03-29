1f6b048efd3896b1b48b8c6f1adb0de0e3a5eaf8
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 06:11:33 -0700

    Fixed indetention

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base  # Import Base from models.base_model
+from models.base_model import BaseModel  # Import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base import Base  # Import Base from models.base
 
 
 class User(BaseModel, Base):
